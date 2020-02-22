90278d842e0f4f8c8f1667f0b01732aa8aabba5b
kylelindy <monsoon443@gmail.com>
2020-02-21 19:40:36 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+### install anaconda
+curl https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh -o ~/Downloads/anaconda_install.sh
+chmod +x ~/Downloads/anaconda_install.sh
+bash ~/Downloads/anaconda_install.sh -b -p $HOME/miniconda
+
+### create python environment
+conda create -n DLEnv1
+
+### activate python environment
+conda activate DLEnv1
+
+### install packages
+conda install nb_conda tensorflow keras matplotlib ipykernel pillow
+
+### register env with jupyter kernel
+ipython kernel install --user --name=DLEnv1
+
+### start notebook
+jupyter notebook
